d4c31ab62a0956183af68bd04a8c193a21e08ec5
JimuelG <jimuelgaas@gmail.com>
2025-03-18 12:10:40 +0800

    v1.012.3

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -1,4 +1,4 @@
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="<?= base_url("/assets/js/jquery.min.js") ?>"></script>
     <script>
         $(document).ready(function(){
             updateOrderTotal();
